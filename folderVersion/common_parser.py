070705aa6359298be080056d51fad10d6fdfcebe
2-kushagra-3 <kushagraid@gmail.com>
2024-07-04 17:12:09 +0530

    Added code to incorporate decimals and megeded the pools with common parser

--- folderVersion/common_parser.py
+++ folderVersion/common_parser.py
@@ -1,9 +1,39 @@
 from enums import MessageField
 
-def parse_common(message, fields_length):
+def add_decimal(value, position):
+    return value[:position] + '.' + value[position:]
+
+def parse_common_with_pools(message, field_lengths, pool_field_lengths=None, decimal_fields=None):
     parsed_message = {}
     start_idx = 0
-    for field, length in fields_length.items():
-        parsed_message[field] = message[start_idx:start_idx + length]
+    num_pools = 0
+
+    if decimal_fields is None:
+        decimal_fields = {}
+
+    for field, length in field_lengths.items():
+        field_value = message[start_idx:start_idx + length]
+        if field in decimal_fields:
+            position = decimal_fields[field]
+            field_value = add_decimal(field_value, position)
+        parsed_message[field] = field_value
+        if field == MessageField.NUMBER_OF_POOLS:
+            num_pools = int(field_value)
         start_idx += length
-    return parsed_message, start_idx
+
+    if pool_field_lengths and num_pools > 0:
+        pools = []
+        for _ in range(num_pools):
+            pool = {}
+            for pool_field, pool_length in pool_field_lengths.items():
+                pool_field_value = message[start_idx:start_idx + pool_length]
+                if pool_field in decimal_fields:
+                    position = decimal_fields[pool_field]
+                    pool_field_value = add_decimal(pool_field_value, position)
+                pool[pool_field] = pool_field_value
+                start_idx += pool_length
+            pools.append(pool)
+        parsed_message['pools'] = pools
+
+    parsed_message[MessageField.END_MESSAGE_INDICATOR] = message[start_idx:]
+    return parsed_message
--- folderVersion/specific_parsers.py
+++ folderVersion/specific_parsers.py
@@ -1,5 +1,5 @@
 from enums import MessageField, MessageType
-from common_parser import parse_common
+from common_parser import parse_common_with_pools
 
 COMMON_FIELD_LENGTHS = {
     MessageField.START_MESSAGE_INDICATOR: 1,
@@ -20,19 +20,19 @@ def parse_AA(message, parsed_messages):
         MessageField.ORIGINAL_SEQUENCE: 5,
         MessageField.END_MESSAGE_INDICATOR: 1
     })
-    parsed_message, _ = parse_common(message, field_lengths)
+    parsed_message = parse_common_with_pools(message, field_lengths)
     parsed_messages.append(parsed_message)
 
 def parse_TX(message, parsed_messages):
-    parsed_message, _ = parse_common(message, COMMON_FIELD_LENGTHS)
+    parsed_message = parse_common_with_pools(message, COMMON_FIELD_LENGTHS)
     parsed_messages.append(parsed_message)
 
 def parse_ON(message, parsed_messages):
-    parsed_message, _ = parse_common(message, COMMON_FIELD_LENGTHS)
+    parsed_message = parse_common_with_pools(message, COMMON_FIELD_LENGTHS)
     parsed_messages.append(parsed_message)
 
 def parse_DK(message, parsed_messages):
-    parsed_message, _ = parse_common(message, COMMON_FIELD_LENGTHS)
+    parsed_message = parse_common_with_pools(message, COMMON_FIELD_LENGTHS)
     parsed_messages.append(parsed_message)
 
 def parse_CC(message, parsed_messages):
@@ -41,42 +41,31 @@ def parse_CC(message, parsed_messages):
         MessageField.FIELD_7: 10,
         MessageField.FIELD_8: 10,
         MessageField.NUMBER_OF_POOLS: 2,
-        MessageField.FIELD_10: 5
+        MessageField.FIELD_10: 12,
     })
-    
-    parsed_message, start_idx = parse_common(message, field_lengths)
-    number_of_pools = int(parsed_message[MessageField.NUMBER_OF_POOLS])
-    
-    pool_fields_length = {
+    pool_field_lengths = {
         MessageField.POOL_FIELD_1: 10,
-        MessageField.POOL_FIELD_2: 10,
+        MessageField.POOL_FIELD_2: 10
     }
-    
-    parsed_message['pools'] = []
-    for i in range(number_of_pools):
-        pool = {}
-        for field, length in pool_fields_length.items():
-            pool[field] = message[start_idx:start_idx + length]
-            start_idx += length
-        parsed_message['pools'].append(pool)
-    
-    # Capture the end message indicator after all pools
-    parsed_message[MessageField.END_MESSAGE_INDICATOR] = message[start_idx:start_idx + 1]
-    
+    decimal_fields = {
+        MessageField.FIELD_8: 9,
+        MessageField.FIELD_10: 10
+    }
+    parsed_message = parse_common_with_pools(message, field_lengths, pool_field_lengths, decimal_fields)
     parsed_messages.append(parsed_message)
 
 def parse_CX(message, parsed_messages):
-    parsed_message, _ = parse_common(message, COMMON_FIELD_LENGTHS)
+    parsed_message = parse_common_with_pools(message, COMMON_FIELD_LENGTHS)
     parsed_messages.append(parsed_message)
 
 def parse_LS(message, parsed_messages):
-    parsed_message, _ = parse_common(message, COMMON_FIELD_LENGTHS)
+    parsed_message = parse_common_with_pools(message, COMMON_FIELD_LENGTHS)
     parsed_messages.append(parsed_message)
 
 def parse_HA(message, parsed_messages):
-    parsed_message, _ = parse_common(message, COMMON_FIELD_LENGTHS)
+    parsed_message = parse_common_with_pools(message, COMMON_FIELD_LENGTHS)
     parsed_messages.append(parsed_message)
 
 def parse_default(message, parsed_messages):
-    parsed_message, _ = parse_common(message, COMMON_FIELD_LENGTHS)
+    parsed_message = parse_common_with_pools(message, COMMON_FIELD_LENGTHS)
     parsed_messages.append(parsed_message)
